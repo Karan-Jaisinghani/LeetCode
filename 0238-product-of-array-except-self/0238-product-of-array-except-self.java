a8ede663aa39fdf2e66c3ac1398fad6736a97edc
Karan Jaisinghani <145903609+Karan-Jaisinghani@users.noreply.github.com>
2026-03-11 19:28:25 +0530

    Time: 2 ms (94.55%), Space: 63.3 MB (98.14%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -4,10 +4,12 @@ class Solution {
        int [] arr1 = new int[nums.length]; 
        int x = nums.length;
        arr[0]=1;
-       arr1[nums.length-1]=1;
+       arr1[x-1]=1;
        for(int i =1;i<x;i++){
         arr[i]=nums[i-1]*arr[i-1];
-        arr1[x-1-i]=nums[x-i]*arr1[x-i];
+      }
+      for(int i=x-2;i>=0;i--){
+        arr1[i]=nums[i+1]*arr1[i+1];
       }
       for(int i=0;i<x;i++){
         nums[i]=arr[i]*arr1[i];
