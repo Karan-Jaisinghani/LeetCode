49862024ba11425a6a47f16adafe5a59dfb4721f
Karan Jaisinghani <145903609+Karan-Jaisinghani@users.noreply.github.com>
2026-03-16 19:52:38 +0530

    Time: 122 ms (73.55%), Space: 99.6 MB (35.79%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -1,12 +1,8 @@
 class Solution {
     public int minSpeedOnTime(int[] nums, double k) {
+        if(k<=nums.length-1) return -1;
         int max = 0;
         int res = 0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>max){
-                max=nums[i];
-            }
-        }
         double l = 1;
         double r = 10000000;
         while(l<=r){
