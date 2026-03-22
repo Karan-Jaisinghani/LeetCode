7367cde99a61f22a7040c57640a24f828bdb9e06
Karan Jaisinghani <145903609+Karan-Jaisinghani@users.noreply.github.com>
2026-03-22 19:43:01 +0530

    Time: 1 ms (99.94%), Space: 46.3 MB (89.12%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,10 +1,21 @@
 class Solution {
-    public int buyChoco(int[] prices, int money) {
-        Arrays.sort(prices);
-        int n = prices[0]+prices[1];
-        if(n<=money){
-            return money-n;
+    public int buyChoco(int[] nums, int money) {
+        int min = Integer.MAX_VALUE;
+        int smin = Integer.MAX_VALUE;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]<min){
+                smin = min;
+                min = nums[i];
+            }
+            else if(nums[i]<smin){
+                smin = nums[i];
+            }
         }
+        int total = min+smin;
+        if(total<=money){
+            return money-total;
+        }
+        
         return money;
     }
 }
